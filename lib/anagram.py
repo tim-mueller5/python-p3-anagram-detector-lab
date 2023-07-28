78513d677024995684898f6b434486a0358dd29e
Tim Mueller <trmueller5@gmail.com>
2023-07-28 11:46:07 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,26 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        self.list = list
+        word_letters = [letters for letters in self.word]
+        end_list = []
+        for item in list:
+            letters = [letter for letter in item]
+            if(sorted(letters) == sorted(word_letters)):
+                end_list.append(item)
+        return end_list
+
+            
+
+                
+
+
+        # if (self.word in list):
+        #     print(self.word)
+        # else:
+        #     return []
